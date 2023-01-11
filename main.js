ebdc629b19c26c829b2c9d051d4af65000efeb74
fonfonbee <117663564+fonfonbee@users.noreply.github.com>
2023-01-12 02:50:46 +0800

    omg you're so nice

---
+++ main.js
@@ -0,0 +1,14 @@
+var username = "facebonkba"; 
+//var: can be used throughout the whole program
+username = 8; //the magic of JavaScripta
+
+let password = "hunter2"; 
+//let: can only be used within a declared scope
+
+const NAME = 1;
+//constant name ALL CAPS is common practice
+
+var aa = 33;
+var bb = 5;
+console.log(aa % bb);//=>3 (% for remainder)
+//can be used to determine odd/even number
